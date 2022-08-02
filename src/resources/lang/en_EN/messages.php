36bfa2ccd31f5e28f5b30aae781c1ab9ef8f00c9
Alessandro Marotta <alessandro.marotta@samag.tech>
2022-08-02 18:40:03 +0200

    Add new message

--- src/resources/lang/en_EN/messages.php
+++ src/resources/lang/en_EN/messages.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'test'  => 'Testing'
+    'confirm_environment_overwrite'      => 'Env is already configured , do you want overwrite? (y|n) ',
+    'no_envirnoment_overwrite'           => 'Env isn\'t overwrite',
+    'question_environment_user'          => 'Tell me your name: ',
+    'environment_user_not_setting'       => 'You don\'t write you name, type it again',
+    'question_environment_lang'          => 'Choose your language',
+    'complete_environment_configuration' => 'Thanks %user%, the configuration is completed',
+    'environment_not_exist'              => 'Enviroment is not configured, please use command core:configure'
 ];
--- src/resources/lang/it_IT/messages.php
+++ src/resources/lang/it_IT/messages.php
@@ -1,5 +1,11 @@
 <?php
 
 return [
-    'test'  => 'Testami'
+    'confirm_environment_overwrite'      => 'La configurazione è gia presente, vuoi sovrascriverla? (y|n) ',
+    'no_envirnoment_overwrite'           => 'La configurazione non è stata sovrascritta',
+    'question_environment_user'          => 'Scrivi il tuo nome: ',
+    'environment_user_not_setting'       => 'Non è stato scritto nessun nome, rinseriscilo',
+    'question_environment_lang'          => 'Scegli la lingua',
+    'complete_environment_configuration' => 'Grazie %user%, la configurazione è stata completata',
+    'environment_not_exist'              => 'L\'ambiente non è stato configurato, per favore utilizza il comando core:configure'
 ];
